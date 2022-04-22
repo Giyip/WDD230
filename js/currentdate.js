7ebfc0c61bf302f79e0a626a1b5ab61b68251d69
Giyip <godwiniyip@zohomail.com>
2022-04-22 11:48:48 +0100

    Formatted the Html, Css, and Js files.

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,31 +1,37 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+document.getElementById("currentdate").textContent =
+  new Date().toLocaleDateString("en-US", options);
 const todaysdate = new Date();
 
 //const daynames = [
-  //"Sunday",
-  //"Monday",
-  //"Tuesday",
-  //"wednesday",
-  //"Thursday",
-  //"Friday",
-  //"Saturday",
-  //"Sunday",
+//"Sunday",
+//"Monday",
+//"Tuesday",
+//"wednesday",
+//"Thursday",
+//"Friday",
+//"Saturday",
+//"Sunday",
 //];
 
 //const months = [
-  //"January",
-  //"February",
-  //"March",
-  //"April",
-  //"May",
-  //"June",
-  //"July",
-  //"August",
-  //"September",
-  //"October",
-  //"November",
-  //"December",
+//"January",
+//"February",
+//"March",
+//"April",
+//"May",
+//"June",
+//"July",
+//"August",
+//"September",
+//"October",
+//"November",
+//"December",
 //];
 //const todaysdate = new Date();
 //const dayName = dayNames[todaysdate.getDate()];
